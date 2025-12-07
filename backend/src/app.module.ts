dd8005f63dba9e64003282a96d9108a7b552fd4e
amine187 <amin.rhouma@gmail.com>
2025-12-07 19:42:26 +0100

    chore: add TypeOrmModule and ConfigModule to app.mpdule.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(typeormConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
