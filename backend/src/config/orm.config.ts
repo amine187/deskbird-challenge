0c0db9b02a8eed6b352388692779297ce652d4cf
amine187 <amin.rhouma@gmail.com>
2025-12-08 09:06:13 +0100

    refactor: update typorm options

---
+++ backend/src/config/orm.config.ts
@@ -0,0 +1,14 @@
+import { DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const baseTypeOrmOptions: TypeOrmModuleOptions & DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: ['error'],
+};
