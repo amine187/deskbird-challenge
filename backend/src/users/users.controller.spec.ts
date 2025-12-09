43884a561090114c78f8070ecccf5f1ee4bfbde1
amine187 <amin.rhouma@gmail.com>
2025-12-09 09:28:02 +0100

    feat: define findAll controller for get users

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserResponseDto } from './dto';
 
-describe('UsersController', () => {
+describe(`Controller: ${UsersController.name}`, () => {
   let controller: UsersController;
+  const mockUsersService = {
+    findAll: jest.fn(),
+  };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return all users', async () => {
+    const mockUsers: UserResponseDto[] = [
+      {
+        id: 10,
+        email: 'john@example.com',
+        firstName: 'john',
+        lastName: 'white',
+        role: 'admin',
+      },
+      {
+        id: 11,
+        email: 'emily@example.com',
+        firstName: 'emily',
+        lastName: 'smith',
+        role: 'user',
+      },
+    ];
+
+    mockUsersService.findAll.mockResolvedValue(mockUsers);
+
+    const result = await controller.findAll();
+
+    expect(mockUsersService.findAll).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(mockUsers);
   });
 });
