5ed8fd11da246bf346bde652f4851f7fdf66406e
amine187 <amin.rhouma@gmail.com>
2025-12-09 20:44:07 +0100

    feat: add local strategy

---
+++ backend/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ValidateUserResponseDto } from '../dto/auth.dto';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  async validate(
+    email: string,
+    password: string,
+  ): Promise<ValidateUserResponseDto> {
+    const user = await this.authService.validateUser({ email, password });
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
