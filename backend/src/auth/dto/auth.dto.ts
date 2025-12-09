0602a62fb95596021088323c0bf28848479918d5
amine187 <amin.rhouma@gmail.com>
2025-12-09 17:53:12 +0100

    feat: add auth.dto

---
+++ backend/src/auth/dto/auth.dto.ts
@@ -0,0 +1,17 @@
+export class AuthPayloadDto {
+  email!: string;
+  password!: string;
+}
+
+export class ValidateUserResponseDto {
+  id!: string;
+  email!: string;
+  firstName?: string;
+  lastName?: string;
+  role: string;
+}
+
+export class LoginResponseDto {
+  accessToken: string;
+  user: ValidateUserResponseDto;
+}
---
+++ backend/src/auth/dto/index.ts
@@ -0,0 +1,1 @@
+export { AuthPayloadDto } from './auth.dto';
