27f2242c9c8e43c381104e297cb0428bdae8b6f8
amine187 <amin.rhouma@gmail.com>
2025-12-09 09:27:31 +0100

    feat: define `findAll`service

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
-describe('UsersService', () => {
+describe(`Service: ${UsersService.name}`, () => {
   let service: UsersService;
+  let mockUsersRepository: { find: jest.Mock };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
+    mockUsersRepository = {
+      find: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUsersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return all users mapped to response DTOs', async () => {
+    const users = [
+      {
+        id: 10,
+        email: 'john@example.com',
+        firstName: 'john',
+        lastName: 'white',
+        role: 'admin',
+        password: 'password',
+      },
+      {
+        id: 11,
+        email: 'emily@example.com',
+        firstName: 'emily',
+        lastName: 'smith',
+        role: 'user',
+        password: 'password',
+      },
+    ];
+
+    mockUsersRepository.find.mockResolvedValue(users);
+
+    const result = await service.findAll();
+
+    expect(mockUsersRepository.find).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([
+      {
+        id: 10,
+        email: 'john@example.com',
+        firstName: 'john',
+        lastName: 'white',
+        role: 'admin',
+      },
+      {
+        id: 11,
+        email: 'emily@example.com',
+        firstName: 'emily',
+        lastName: 'smith',
+        role: 'user',
+      },
+    ]);
   });
 });
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { Repository } from 'typeorm';
+import { UserResponseDto } from './dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  private mapUserToResponseDto(user: User): UserResponseDto {
+    const { id, email, firstName, lastName, role } = user;
+
+    return { id, email, firstName, lastName, role };
+  }
+
+  async findAll(): Promise<UserResponseDto[]> {
+    const users = await this.usersRepository.find();
+
+    return users.map((user) => this.mapUserToResponseDto(user));
+  }
+}
