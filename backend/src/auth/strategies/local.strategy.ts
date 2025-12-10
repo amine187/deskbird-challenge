adeca8808c41f1a574d6632c85ee7fcd346e951e
amine187 <amin.rhouma@gmail.com>
2025-12-10 08:08:21 +0100

    lint: delete disable lint

--- backend/src/auth/strategies/local.strategy.ts
+++ backend/src/auth/strategies/local.strategy.ts
@@ -7,7 +7,6 @@ import { ValidateUserResponseDto } from '../dto/auth.dto';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({
       usernameField: 'email',
     });
