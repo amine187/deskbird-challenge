19c92d2277071702cc170708ace6e0bb42830b8a
amine187 <amin.rhouma@gmail.com>
2025-12-10 08:07:41 +0100

    feat: add dto strategy

---
+++ backend/src/auth/dto/strategy.dto.ts
@@ -0,0 +1,14 @@
+export class ValidateJwtStrategyPayloadDto {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  iat: number;
+  exp: number;
+}
+
+export class ValidateJwtStrategyResponseDto {
+  id!: string;
+  email!: string;
+}
