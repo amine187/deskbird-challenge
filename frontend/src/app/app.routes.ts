7e416813c7f7d29b97bc859c17c88f1b3730024f
amine187 <amin.rhouma@gmail.com>
2025-12-11 09:31:04 +0100

    feat: update app.ts to support new routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Layout } from './core';
+import { AuthGuard, NoAuthGuard } from './core/guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    canActivate: [NoAuthGuard],
+    loadChildren: () => import('./auth/auth.routes').then((mod) => mod.AUTH_ROUTES),
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'users',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./user/user.routes').then((mod) => mod.USER_ROUTES),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
